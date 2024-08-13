415d327be9aff40a7929a4c7e3e4702bbf2089bf
jd8af <josedavid8af@gmail.com>
2024-08-13 17:51:27 +0000

    normalize

--- Kaon_B_Variables/loop_plot.py
+++ Kaon_B_Variables/loop_plot.py
@@ -8,13 +8,14 @@ bsm_tuple = file['B']
 df = ROOT.RDataFrame('Tuple', bsm_tuple)
 
 variables = ['B_BKGCAT','B_END_VZ','B_END_VX','B_END_VY','B_END_VRHO','B_BPVCORRM','B_BPVDIRA','B_BPVETA','B_BPVIPCHI2','B_BPVFDCHI2','B_BPVLTIME','B_BPVFD','B_BPVFDIRX','B_BPVFDIRY','B_BPVFDIRZ','B_BPVFDVECX','B_BPVFDVECY','B_BPVFDVECZ','B_BPVIP','B_BPVVDRHO','B_BPVVDX','B_BPVVDY','B_BPVVDZ', 'B_M','B_P','B_PT','B_PX','B_PY','B_PZ','B_ENERGY','B_ETA','K_plus_MASS','K_plus_CHI2','K_plus_M','K_plus_P','K_plus_PT','K_plus_PX','K_plus_PY','K_plus_PZ','K_plus_ENERGY','K_plus_ETA','K_plus_MINIP','K_plus_MINIPCHI2','K_plus_QOVERP','K_plus_ECALPIDE','K_plus_ECALPIDMU','K_plus_ELECTRONENERGY','K_plus_ELECTRONID','K_plus_ELECTRONSHOWEREOP','K_plus_HCALEOP','K_plus_HCALPIDE','K_plus_HCALPIDMU','K_plus_TX','K_plus_TY','K_plus_BPVX','K_plus_BPVY','K_plus_BPVZ','K_plus_CHARGE','K_plus_GHOSTPROB','K_plus_PID_P','K_plus_PID_PI','K_plus_PID_K','K_plus_PID_MU','K_plus_PID_E','H_M']
-
+#variables =['B_BKGCAT','B_M', 'B_P','K_plus_M','K_plus_P']
 tuple_array = df.AsNumpy(variables)
 
 bkgcat_100_mask = tuple_array['B_BKGCAT'] == 100
 bkgcat_70_mask = tuple_array['B_BKGCAT'] == 70
 bkgcat_30_mask = tuple_array['B_BKGCAT'] == 30
 
+factor = 1
 for variable in variables[1:]:  # Skip 'B_BKGCAT' for plotting
     bck_array = tuple_array[variable][bkgcat_100_mask]
     set_array = tuple_array[variable][bkgcat_70_mask]
@@ -37,11 +38,15 @@ for variable in variables[1:]:  # Skip 'B_BKGCAT' for plotting
     hist1.SetLineColor(ROOT.kRed)
     hist2.SetLineColor(ROOT.kBlue)
     hist3.SetLineColor(ROOT.kGreen)
-
+   
+    hist1.Scale(factor/(hist1.Integral()))
+    hist2.Scale(factor/(hist2.Integral()))
+    hist3.Scale(factor/(hist3.Integral()))
+   
     c1 = ROOT.TCanvas("c1", f"Histogram Canvas - {variable}", 800, 600)
-    hist1.Draw()
-    hist2.Draw("SAME")
-    hist3.Draw("SAME")
+    hist1.Draw('HIST')
+    hist2.Draw('HIST SAME')
+    hist3.Draw('HIST SAME')
 
     legend = ROOT.TLegend(0.7, 0.7, 0.9, 0.9)
     legend.AddEntry(hist1, "bkgcat=100", "l")
